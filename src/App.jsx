9c2e09748644f2dcc555b866ba2b3ddd99cb7791
omk963 <132976939+omk963@users.noreply.github.com>
2024-06-16 12:33:35 -0700

    added an array of 3 todo list items and printed to screen

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,34 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const todoList = [
+  {
+    id: 1,
+    title: 'Complete week 1 assignment'
+  },
+  {
+    id: 2,
+    title: 'Complete week 2 assignment'
+  },
+  {
+    id: 3,
+    title: 'Complete week 3 assignment'
+  }
+];
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Todo-List</h1>
+      <ul>
+        {todoList.map((todo) => {
+          return (
+            <li>{`${todo.id}) ${todo.title}`}</li>
+          );
+        })}
+      </ul>
     </>
   )
 }
