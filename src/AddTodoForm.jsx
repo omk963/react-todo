febaa41d205630173695510bc23ab034d3b29791
omk963 <132976939+omk963@users.noreply.github.com>
2024-06-30 11:51:59 -0700

    feat(todo): refactor and enhance Todo List components

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,8 +1,16 @@
-const AddTodoForm = () => {
+const AddTodoForm = ({ onAddTodo }) => {
+    const handleAddTodo = (event) => {
+        event.preventDefault();
+        const todoTitle = event.target.elements.title.value;
+        console.log(todoTitle);
+        onAddTodo(todoTitle);
+        event.target.elements.title.value = '';
+    };
+
     return (
-        <form>
+        <form onSubmit={handleAddTodo}>
             <label>Title</label>
-            <input id='todoTitle' type='text' />
+            <input id='todoTitle' type='text' name='title' />
             <label htmlFor="todoTitle"></label>
             <button type="submit">Add</button>
         </form>
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import './App.css'
+import { useState } from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
 function App() {
+  const [newTodo, setNewTodo] = useState();
+
   return (
     <>
       <h1>Todo-List</h1>
-      <AddTodoForm />
+      <AddTodoForm onAddTodo={setNewTodo} />
+      <p>{newTodo}</p>
       <TodoList />
     </>
-  )
-}
+  );
+};
 
 export default App;
