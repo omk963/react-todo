1767a7e3a51f6096c9372e2e2f801268b3b6082b
Jin <tcofte7827@gmail.com>
2024-07-24 21:12:46 -0700

    feat(todos): Updated todoList to take in variables instead of hard-coded ones

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,16 +1,27 @@
+import { useState } from 'react';
+
 const AddTodoForm = ({ onAddTodo }) => {
+    const [todoTitle, setTodoTitle] = useState('');
+
+    const handleTitleChange = (event) => {
+        const newTodoTitle = event.target.value;
+        setTodoTitle(newTodoTitle);
+    };
+
     const handleAddTodo = (event) => {
         event.preventDefault();
-        const todoTitle = event.target.elements.title.value;
         console.log(todoTitle);
-        onAddTodo(todoTitle);
-        event.target.elements.title.value = '';
+        onAddTodo({
+            title: todoTitle,
+            id: Date.now()
+        });
+        setTodoTitle('');
     };
 
     return (
         <form onSubmit={handleAddTodo}>
             <label>Title</label>
-            <input id='todoTitle' type='text' name='title' />
+            <input id='todoTitle' type='text' name='title' value={todoTitle} onChange={handleTitleChange} />
             <label htmlFor="todoTitle"></label>
             <button type="submit">Add</button>
         </form>
