c7642dbe81754402fb013a0d23114cb610af5558
omk963 <132976939+omk963@users.noreply.github.com>
2024-07-26 21:07:16 -0700

    feat(todos): Implemented useEffect and Created custom hook and applied it

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
 import './App.css'
-import { useState } from 'react';
+import React from 'react';
+import { useState, useEffect } from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
+const useSemiPersistentState = () => {
+  const [todoList, setTodoList] = useState(() => {
+    const savedTodos = localStorage.getItem('savedTodoList');
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+  }, [todoList])
+
+  return [todoList, setTodoList]
+};
+
 function App() {
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useSemiPersistentState();
 
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo])
   };
 
   return (
-    <>
+    <React.Fragment>
       <h1>Todo-List</h1>
       <AddTodoForm onAddTodo={addTodo} />
-      <TodoList  todoList={todoList} />
-    </>
+      <TodoList todoList={todoList} />
+    </React.Fragment>
   );
 };
 
