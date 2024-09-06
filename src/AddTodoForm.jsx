b58dfeb3b33e4f214eb519a14fbfcd4849628f82
omk963 <132976939+omk963@users.noreply.github.com>
2024-09-05 21:16:21 -0700

    refactor(todo): resolved merge conflict

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,9 +1,5 @@
 import { useState } from 'react';
-<<<<<<< HEAD
 import style from './TodoListItem/TodoListItem.module.css'
-=======
-import InputWithLabel from './InputWithLabel';
->>>>>>> 5f22878ac82a99e52bfdf6923a0eefc4f0de6cbf
 
 const AddTodoForm = ({ onAddTodo }) => {
     const [todoTitle, setTodoTitle] = useState('');
@@ -29,17 +25,10 @@ const AddTodoForm = ({ onAddTodo }) => {
 
     return (
         <form onSubmit={handleAddTodo}>
-<<<<<<< HEAD
             <label className={style.title}>Title</label>
             <input className={style.todo} id='todoTitle' type='text' name='title' value={todoTitle} onChange={handleTitleChange} />
             <label htmlFor="todoTitle"></label>
             <button type="submit" className={style.addBtn}>Add</button>
-=======
-            <InputWithLabel id='todoTitle' value={todoTitle} onInputChange={handleTitleChange}>
-                Title:
-            </InputWithLabel>
-            <button type="submit">Add</button>
->>>>>>> 5f22878ac82a99e52bfdf6923a0eefc4f0de6cbf
         </form>
     );
 };
